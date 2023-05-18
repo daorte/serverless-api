51141d925a87d5dcf19b322ee21cba4efbcb2e98
David <daorte@gmail.com>
2023-05-18 09:58:59 +0200

    Update app.js

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello from GitHub',
+                message: 'Hello from GitHub',
                 // location: ret.data.trim()
             })
         }
